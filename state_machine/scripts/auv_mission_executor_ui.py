1b28c68ac6afc4297938ad061c59603e06fd438c
Nils Bore <nilsbore@gmail.com>
2018-09-26 17:10:50 +0200

    Got the first version of the task ui working

---
+++ state_machine/scripts/auv_mission_executor_ui.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import roslib
+import rospy
+
+import auv_mission_executor.page_utils
+
+from smarc_msgs.msg import SMTask, ExecutionStatus # TaskEvent
+import strands_webserver.client_utils
+import strands_webserver.page_utils
+from std_srvs.srv import Empty
+
+if __name__ == '__main__':
+    rospy.init_node("auv_mission_executor_ui")
+
+    # display some content
+    display_no = rospy.get_param("~display", 0)
+
+    if display_no == 0:
+        rospy.loginfo('writing to all displays)')
+    else:
+        rospy.loginfo('writing to display: %s', display_no)
+
+    prefix='auv_mission_executor_ui_server'
+    
+    # Setup -- must be done before other strands_admin_web_ui calls
+    # serves pages relative to strands_admin_web_ui/www -- this is important as there as some javascript files there
+    strands_webserver.client_utils.set_http_root(roslib.packages.get_pkg_dir('state_machine') + '/www', prefix=prefix)
+    rospy.loginfo('root set')
+
+    page = 'index.html'
+
+    right_div = '<div id="top_right_div"></div><div id="bottom_right_div"><div id="button_left_div" style="width:300px; float:left;"></div><div id="button_right_div" style="width:300px; float:left;"></div></div>'
+    script = auv_mission_executor.page_utils.get_schedule_display("#top_right_div")
+    script += auv_mission_executor.page_utils.get_service_button("Cancel active task", "sm_reset", "#button_left_div")
+    script += auv_mission_executor.page_utils.get_service_button("Cancel all tasks", "cancel_all_tasks", "#button_right_div")
+
+    auv_mission_executor.page_utils.generate_interface_page(page, right=right_div, script=script)
+    rospy.loginfo('page generated')
+
+    strands_webserver.client_utils.display_relative_page(display_no, page, prefix=prefix)
+    rospy.spin()
---
+++ state_machine/src/auv_mission_executor/page_utils.py
@@ -0,0 +1,65 @@
+import roslib
+import rospy
+import web
+import os
+
+import strands_webserver.client_utils
+import strands_webserver.page_utils
+
+from smarc_msgs.msg import SMTask, ExecutionStatus
+
+#from strands_admin_web_ui.services import TaskDemander
+#import strands_admin_web_ui.page_utils
+
+
+template_dir = roslib.packages.get_pkg_dir('state_machine') + '/templates'
+render = web.template.render(template_dir)
+www_prefix = roslib.packages.get_pkg_dir('state_machine') + '/www/'
+
+def generate_interface_page(file_name, left = "", right = "", script = ""):
+    """ Create a page by rendering the strands_admin_web_ui/templates/index.html using web.py and place it at strands_admin_web_ui/www/ """
+
+    full_path = www_prefix + file_name
+    rospy.loginfo('generate page %s' % full_path)
+
+    try:
+        os.makedirs(os.path.dirname(full_path))
+    except OSError:
+        pass
+
+    page = str(render.index(left, right, script))
+    with open(full_path, 'w+') as f:
+        f.write(page)
+
+
+def get_schedule_display(element="#left_div"):
+    """ Generate the javascript to be which displays a schedule display in the named div element. """
+    return str(render.schedule_display(element))
+
+#def get_map_display(element="bottom_left"):
+#    """ Generate the javascript to be which displays a schedule display in the named div element. """
+#    return str(render.map(element))
+
+#def get_video_display(element="bottom_right"):
+#    """ Generate the javascript to be which displays a schedule display in the named div element. """
+#    return str(render.mjpeg_stream(element))
+
+#def get_task_event_display(element="#left_div"):
+#    """ Generate the javascript to be which displays task event details  in the named div element. """
+#    return str(render.task_event_display(element))
+
+def get_service_button(button_text, service, element="#bottom_right_div"):
+    """ Generate a button that calls the std_srvs/Empty service when pressed """
+    print "Adding a service button!"
+    return str(render.service_button(button_text, service, element))
+
+#def generate_buttons_for_tasks(tasks_and_labels):
+#    demander = TaskDemander()
+#
+#    output = ""
+#
+#    for task, label in tasks_and_labels:
+#        demand_srv = demander.offer_demand_service(task)
+#        output += strands_admin_web_ui.page_utils.get_service_button(label, demand_srv)
+#
+#    return output, demander
