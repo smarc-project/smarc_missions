778061d08ea2bb72a416ca173ea9f1667fdea0d9
Nils Bore <nilsbore@gmail.com>
2018-10-02 10:32:47 +0200

    Renamed the packages

--- auv_sm_mission_executor/setup.py
+++ auv_sm_mission_executor/setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  scripts=[ 'scripts/smach_sm.py' ]
+  scripts=[ 'scripts/smach_sm.py' ],
+  packages=['auv_sm_mission_executor'],
+  package_dir={'': 'src'})
 )
 
 setup(**d)
