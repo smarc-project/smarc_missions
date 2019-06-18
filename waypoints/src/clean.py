2027e92655a3ad38c066fe11566bb5a680ecb244
sprague <sprague@kth.se>
2019-06-18 20:08:09 +0200

    json cleaner working

--- waypoints/src/clean.py
+++ waypoints/src/clean.py
@@ -3,7 +3,7 @@
 
 import json
 
-def clean(fname):
+def clean(fname, sfname=None):
 
     # load the pretty json file
     f = open(fname, 'r').read()
@@ -18,9 +18,16 @@ def clean(fname):
     # remove
     f = f.split(',"transitions":')[0]
     f = f.split('"maneuvers":')[1]
-    #f = f.replace('\n', '')
+    f = f.replace('\n', '')
 
-    print(f)
+    # convert to json
+    f = json.loads(f)
+
+    # save
+    if sfname is not None:
+        with open(sfname, 'w') as sf:
+            json.dump(f, sf, sort_keys=True, indent=4)
+
 
 if __name__ == "__main__":
-    clean('plan.json')
+    clean('plan.json', 'betterplan.json')
