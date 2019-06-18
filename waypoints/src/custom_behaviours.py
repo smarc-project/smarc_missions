81ba80222818e0640164b83c77a75938e7f7b62b
sprague <sprague@kth.se>
2019-06-18 20:18:36 +0200

    Added reactive sequence and running blackboard setter

---
+++ waypoints/src/custom_behaviours.py
@@ -0,0 +1,88 @@
+# Chrisotpher Iliffe Sprague
+# sprague@kth.se
+
+import py_trees as pt, itertools
+
+class Sequence(pt.composites.Selector):
+
+    """
+    Reactive sequence overidding sequence with memory, py_trees' only available sequence.
+    """
+
+    def __init__(self, name="Sequence", children=None):
+        super(Sequence, self).__init__(name=name, children=children)
+
+    def tick(self):
+        """
+        Run the tick behaviour for this selector. Note that the status
+        of the tick is always determined by its children, not
+        by the user customised update function.
+        Yields:
+            :class:`~py_trees.behaviour.Behaviour`: a reference to itself or one of its children
+        """
+        self.logger.debug("%s.tick()" % self.__class__.__name__)
+        # Required behaviour for *all* behaviours and composites is
+        # for tick() to check if it isn't running and initialise
+        if self.status != pt.common.Status.RUNNING:
+            # selectors dont do anything specific on initialisation
+            #   - the current child is managed by the update, never needs to be 'initialised'
+            # run subclass (user) handles
+            self.initialise()
+        # run any work designated by a customised instance of this class
+        self.update()
+        previous = self.current_child
+        for child in self.children:
+            for node in child.tick():
+                yield node
+                if node is child:
+                    if node.status == pt.common.Status.RUNNING or node.status == pt.common.Status.FAILURE:
+                        self.current_child = child
+                        self.status = node.status
+                        if previous is None or previous != self.current_child:
+                            # we interrupted, invalidate everything at a lower priority
+                            passed = False
+                            for child in self.children:
+                                if passed:
+                                    if child.status != pt.common.Status.INVALID:
+                                        child.stop(pt.common.Status.INVALID)
+                                passed = True if child == self.current_child else passed
+                        yield self
+                        return
+        # all children succeded, set succed ourselves and current child to the last bugger who failed us
+        self.status = pt.common.Status.SUCCESS
+        try:
+            self.current_child = self.children[-1]
+        except IndexError:
+            self.current_child = None
+        yield self
+
+
+class SetBlackboardVariable(pt.behaviours.Running):
+    """
+    Set the specified variable on the blackboard.
+    Usually we set variables from inside other behaviours, but can
+    be convenient to set them from a behaviour of their own sometimes so you
+    don't get blackboard logic mixed up with more atomic behaviours.
+    Args:
+        name (:obj:`str`): name of the behaviour
+        variable_name (:obj:`str`): name of the variable to set
+        variable_value (:obj:`any`): value of the variable to set
+    .. todo:: overwrite option, leading to possible failure/success logic.
+    """
+    def __init__(self,
+                 name="Set Blackboard Variable",
+                 variable_name="dummy",
+                 variable_value=None
+                 ):
+        """
+        :param name: name of the behaviour
+        :param variable_name: name of the variable to set
+        :param value_name: value of the variable to set
+        """
+        super(SetBlackboardVariable, self).__init__(name)
+        self.variable_name = variable_name
+        self.variable_value = variable_value
+
+    def initialise(self):
+        self.blackboard = pt.blackboard.Blackboard()
+        self.blackboard.set(self.variable_name, self.variable_value, overwrite=True)
