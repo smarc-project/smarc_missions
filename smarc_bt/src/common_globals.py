f5a72e30c4c910c6011c44491d07b27e33a7dd55
Ozer <ozero@kth.se>
2020-05-19 23:35:29 +0200

    More disentanglement

---
+++ smarc_bt/src/common_globals.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# Ozer Ozkahraman (ozero@kth.se)
+
+
+########################
+# DEFAULT VALUES
+########################
+# in ms.
+BT_TICKING_PERIOD = 50
+
+# these are from croatia, biograd coast
+DEFAULT_UTM_ZONE = 33
+DEFAULT_UTM_BAND = 'T'
+# if set to true, utm zone and band will be set from
+# the gps fix we read instead of rosparams
+TRUST_GPS = True
