bdaf47dbef57911217ecb417284008d99443a13c
Ozer Ozkahraman <ozero@kth.se>
2020-06-05 18:58:12 +0200

    Added none check to set next plan action

--- smarc_bt/src/bt_actions.py
+++ smarc_bt/src/bt_actions.py
@@ -238,6 +238,10 @@ class A_SetNextPlanAction(pt.behaviour.Behaviour):
 
     def update(self):
         mission_plan = self.bb.get(bb_enums.MISSION_PLAN_OBJ)
+        if mission_plan is None:
+            rospy.logwarn_throttle("Mission plan was None!")
+            return pt.Status.FAILURE
+
         if not self.do_not_visit:
             mission_plan.visit_wp()
         next_action = mission_plan.get_current_wp()
