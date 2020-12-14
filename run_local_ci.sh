c67fc415d33fd33b66579a7af098cdfb50c947fb
Nils Bore <nilsbore@gmail.com>
2020-12-14 13:24:45 +0100

    Fixes for ci

--- run_local_ci.sh
+++ run_local_ci.sh
@@ -1,1 +1,1 @@
-rosrun industrial_ci run_ci ROS_DISTRO=melodic ROS_REPO=main
+rosrun industrial_ci run_ci ROS_DISTRO=melodic ROS_REPO=main UPSTREAM_WORKSPACE='smarc_missions.rosinstall -smarc_missions'
