5e7e7721f8ab46e7ec8d80c8afc5405c69fe552d
Ozer Ozkahraman <ozero@kth.se>
2021-05-05 14:38:44 +0200

    added bt_common to viminorder

--- smarc_bt/src/viminorder.sh
+++ smarc_bt/src/viminorder.sh
@@ -1,1 +1,1 @@
-vim smarc_bt.py bt_actions.py bt_conditions.py mission_plan.py auv_config.py bb_enums.py imc_enums.py common_globals.py ../launch/smarc_bt.launch ../../bt_mission/launch/mission.launch
+vim smarc_bt.py bt_actions.py bt_conditions.py bt_common.py mission_plan.py auv_config.py bb_enums.py imc_enums.py common_globals.py ../launch/smarc_bt.launch ../../bt_mission/launch/mission.launch
