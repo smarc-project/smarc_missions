dfc011eff43eb87ca27a092eb7242b24ddefc2ab
Ozer Ozkahraman <ozero@kth.se>
2021-06-02 17:45:42 +0200

    lolo changes?

--- smarc_bt/src/mission_log.py
+++ smarc_bt/src/mission_log.py
@@ -135,7 +135,7 @@ class MissionLog:
         return True
 
 
-    def log(self, mplan, bb, t=None):
+    def log(self, bb, mplan, t=None):
         ############################################
         # vehicle-specific stuff
         logged_lolo = self.log_lolo(bb)
@@ -170,7 +170,7 @@ class MissionLog:
             gps_utm_point = None
         else:
             # translate the latlon to utm point using the same service as the mission plan
-            gps_utm_x, gps_utm_y = mplan.latlon_to_utm(gps.latitude, gps.lonitude)
+            gps_utm_x, gps_utm_y = mplan.latlon_to_utm(gps.latitude, gps.longitude)
             if gps_utm_x is None or gps_utm_y is None:
                 gps_utm_point = None
         self.raw_gps_trace.append(gps_utm_point)
