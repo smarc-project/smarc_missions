8c0248c0c53b87e8af2995e446ef99eae1336b02
Ozer Ozkahraman <ozero@kth.se>
2023-03-31 16:34:15 +0200

    added lolo action servers package, with the barebones scaffolding to control lolo2

---
+++ lolo_action_servers/scripts/lolo.py
@@ -0,0 +1,122 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+# Copyright 2023 Ozer Ozkahraman (ozero@kth.se)
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+from __future__ import division, print_function
+import numpy as np
+import tf
+
+class Lolo(object):
+    def __init__(self):
+        """
+        A container object that abstracts away ros-related stuff for a nice abstract vehicle
+        """
+
+        self.pos = np.zeros(3)
+        self.desired_pos = np.zeros(3)
+
+        self.ori_rpy = np.zeros(3)
+
+        self.thruster_rpms = np.zeros(2)
+        self.desired_rpms = np.zeros(2)
+
+        self.elevon_angles = np.zeros(2)
+        self.desired_elevon_angles = np.zeros(2)
+
+        self.rudder_angle = 0
+        self.desired_rudder_angle = 0
+
+        self.elevator_angle = 0
+        self.desired_elevator_angle = 0
+
+
+    def update_pos(self, x=None, y=None, depth=None):
+        if x is not None: self.pos[0] = x
+        if y is not None: self.pos[1] = y
+        if depth: self.pos[2] = depth
+
+    def update_ori(self, r=None, p=None, y=None):
+        if r is not None: self.ori_rpy[0] = r
+        if p is not None: self.ori_rpy[1] = p
+        if y is not None: self.ori_rpy[2] = y
+
+    def update_thruster_rpms(self, port=None, strb=None):
+        if port is not None: self.thruster_rpms[0] = port
+        if strb is not None: self.thruster_rpms[1] = strb
+
+    def update_elevator_angle(self, a):
+        self.elevator_angle = a
+
+    def update_elevon_angles(self, port=None, strb=None):
+        if port is not None: self.elevon_angles[0] = port
+        if strb is not None: self.elevon_angles[1] = strb
+
+    def update_rudder_angle(self, a):
+        self.rudder_angle = a
+
+    def update_elevator_angle(self, a):
+        self.elevator_angle = a
+
+
+    def blarg(self):
+        self.desired_elevator_angle = np.random.standard_normal()*0.6
+        self.desired_rudder_angle = np.random.standard_normal()*0.6
+        self.desired_elevon_angles[0] = np.random.standard_normal()*0.6
+        self.desired_elevon_angles[1] = np.random.standard_normal()*0.6
+        self.desired_rpms[0] = np.random.standard_normal()*2000
+        self.desired_rpms[1] = np.random.standard_normal()*2000
+
+
+    ###############################
+    ### Properties for convenience
+    ###############################
+    @property
+    def x(self):
+        return self.pos[0]
+    @property
+    def y(self):
+        return self.pos[1]
+    @property
+    def depth(self):
+        return self.pos[2]
+    @property
+    def roll(self):
+        return self.ori_rpy[0]
+    @property
+    def pitch(self):
+        return self.ori_rpy[1]
+    @property
+    def yaw(self):
+        return self.ori_rpy[2]
+    @property
+    def ori_quat(self):
+        return tf.transformations.quaternion_from_euler(self.roll, self.pitch, self.yaw)
+    @property
+    def port_rpm(self):
+        return self.thruster_rpms[0]
+    @property
+    def strb_rpm(self):
+        return self.thruster_rpms[1]
+    @property
+    def port_elevon_angle(self):
+        return self.elevon_angles[0]
+    @property
+    def strb_elevon_angle(self):
+        return self.elevon_angles[1]
+
+
+
+
---
+++ lolo_action_servers/scripts/ros_lolo.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+# Copyright 2023 Ozer Ozkahraman (ozero@kth.se)
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+from __future__ import division, print_function
+import numpy as np
+import rospy
+
+from smarc_msgs.msg import ThrusterFeedback, ThrusterRPM
+from std_msgs.msg import Float32
+
+class ROSLolo(object):
+    def __init__(self,
+                 lolo,
+                 robot_name="lolo",
+                 control_thrusters=True,
+                 control_elevons=True,
+                 control_rudder=True,
+                 control_elevator=True):
+        """
+        Reads the state of a lolo from ROS topics and updates the given lolo object
+        Reads the desired angles and such from the lolo object and publishes setpoints
+        """
+        self.lolo = lolo
+        robot_name = "/"+robot_name
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.update)
+
+        self.control_thrusters = control_thrusters
+        if control_thrusters:
+            self.t1_sub = rospy.Subscriber(robot_name+"/core/thruster1_fb", ThrusterFeedback, self.t1_cb, queue_size=1)
+            self.t2_sub = rospy.Subscriber(robot_name+"/core/thruster2_fb", ThrusterFeedback, self.t2_cb, queue_size=1)
+            self.t1_pub = rospy.Publisher(robot_name+"/core/thruster1_cmd", ThrusterRPM, queue_size=1)
+            self.t2_pub = rospy.Publisher(robot_name+"/core/thruster2_cmd", ThrusterRPM, queue_size=1)
+
+        self.control_elevons = control_elevons
+        if control_elevons:
+            self.elevon_p_sub = rospy.Subscriber(robot_name+"/core/elevon_port_fb", Float32, self.elevon_p_cb, queue_size=1)
+            self.elevon_s_sub = rospy.Subscriber(robot_name+"/core/elevon_strb_fb", Float32, self.elevon_s_cb, queue_size=1)
+            self.elevon_p_pub = rospy.Publisher(robot_name+"/core/elevon_port_cmd", Float32, queue_size=1)
+            self.elevon_s_pub = rospy.Publisher(robot_name+"/core/elevon_strb_cmd", Float32, queue_size=1)
+
+        self.control_rudder = control_rudder
+        if control_rudder:
+            self.rudder_sub = rospy.Subscriber(robot_name+"/core/rudder_fb", Float32, self.rudder_cb, queue_size=1)
+            self.rudder_pub = rospy.Publisher(robot_name+"/core/rudder_cmd", Float32, queue_size=1)
+
+        self.control_elevator = control_elevator
+        if control_elevator:
+            self.elevator_sub = rospy.Subscriber(robot_name+"/core/elevator_fb", Float32, self.elevator_cb, queue_size=1)
+            self.elevator_pub = rospy.Publisher(robot_name+"/core/elevator_cmd", Float32, queue_size=1)
+
+    def stop(self):
+        rospy.loginfo("Stopping lolo controller")
+        self.timer.shutdown()
+
+    def update(self, timer_event):
+        if self.control_thrusters:
+            self.t1_pub.publish(int(self.lolo.desired_rpms[0]))
+            self.t2_pub.publish(int(self.lolo.desired_rpms[1]))
+
+        if self.control_elevons:
+            self.elevon_p_pub.publish(self.lolo.desired_elevon_angles[0])
+            self.elevon_s_pub.publish(self.lolo.desired_elevon_angles[1])
+
+        if self.control_rudder:
+            self.rudder_pub.publish(self.lolo.desired_rudder_angle)
+
+        if self.control_elevator:
+            self.elevator_pub.publish(self.lolo.desired_elevator_angle)
+
+    def t1_cb(self, msg):
+        self.lolo.update_thruster_rpms(port=msg.rpm.rpm)
+
+    def t2_cb(self, msg):
+        self.lolo.update_thruster_rpms(strb=msg.rpm.rpm)
+
+    def elevon_p_cb(self, msg):
+        self.lolo.update_elevon_angles(port=msg.data)
+
+    def elevon_s_cb(self, msg):
+        self.lolo.update_elevon_angles(strb=msg.data)
+
+    def rudder_cb(self, msg):
+        self.lolo.update_rudder_angle(msg.data)
+
+    def elevator_cb(self, msg):
+        self.lolo.update_elevator_angle(msg.data)
+
+
+
+
+
+
